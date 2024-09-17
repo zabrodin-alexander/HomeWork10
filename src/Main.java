d34aade21f9510feaefaeb6ce9b4fa28dfb1a5ac
Zabro <zabrodin.alexander51@gmail.com>
2024-09-17 21:40:11 +0300

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task 1
+        System.out.println("Task 1");
+
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О сотрудника — " + fullName);
+
+        //task 2
+        System.out.println("Task 2");
+        System.out.println("Данные Ф.И.О сотрудника для заполнения отчета — " +
+                fullName.toUpperCase());
+
+        //task 3
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println("Данные Ф.И.О сотрудника — " + fullName);
+
     }
 }
